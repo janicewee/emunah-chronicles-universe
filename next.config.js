4794b284c28870e4708d5ef5be4dba68ed49e750
Your GitHub janicewee <janiceweety@gmail.com>
2026-01-17 17:06:24 +0800

    Add explicit webpack alias for @ path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const path = require("node:path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
